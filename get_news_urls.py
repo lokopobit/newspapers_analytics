aa9d32c2d0791b0477b590525b811a64384cf2c0
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-05-30 12:54:17 +0200

    Change: parsing
    The parsing was not working as expected. Now the desired urls are correctly parsed.

--- get_news_urls.py
+++ get_news_urls.py
@@ -14,36 +14,43 @@ prensa_autonomica = 'http://www.tnrelaciones.com/anexo/prensa_andalucia/index.ht
 
 html_es = requests.get(prensa_espannola).text
 soup_es = BeautifulSoup(html_es, features="lxml")
-classes_es = soup_es.find_all('h4') ; classes_es = [ae.text for ae in classes_es]
+
 auxs = soup_es.find(id = 'enlaces-extras-col-izq') ; data_es1 = list(auxs.find("h4").next_siblings)
 classes_es1 = auxs.find_all('h4') ; classes_es1 = [ae.text for ae in classes_es1]
-auxs = soup_es.find(id = 'enlaces-extras-col-der') ; data_es2 = list(auxs.find("h4").next_siblings)
+
+auxs = soup_es.find(id = 'enlaces-extras-col-der') ; 
+while True:
+    try:
+        auxs.find('div', {'class':'publi-google-extras'}).decompose()
+    except:
+        break
+data_es2 = list(auxs)
 classes_es2 = auxs.find_all('h4') ; classes_es2 = [ae.text for ae in classes_es2]
+classes_es2 = classes_es1[-1:] + classes_es2
 
 urls_dict = {}
-aux = []
 i = 0
+urls_dict[classes_es1[i]] = []
 for data in data_es1:
     if data.name == 'h4':
-        aux = []
         i += 1
+        urls_dict[classes_es1[i]] = []
     
     try:
-        aux.append(data.text +'_'+ data.a['href'])
-        urls_dict[classes_es1[i]] = aux
+        aux = data.text +'_'+ data.a['href']
+        urls_dict[classes_es1[i]].append(aux)
     except:
         continue
     
-aux = []
 i = 0
 for data in data_es2:
     if data.name == 'h4':
-        aux = []
         i += 1
+        urls_dict[classes_es2[i]] = []
     
     try:
-        aux.append(data.text +'_'+ data.a['href'])
-        urls_dict[classes_es2[i]] = aux
+        aux = data.text +'_'+ data.a['href']
+        urls_dict[classes_es2[i]].append(aux)
     except:
         continue
     
