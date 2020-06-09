1c28f55bcc479a71ba3647bbeb7922d4821849b7
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-06-09 19:24:54 +0200

    Add: check wether json keys are the same all along the scrapped articles.

--- get_news.py
+++ get_news.py
@@ -7,9 +7,11 @@ Created on Sat May 23 16:55:43 2020
 
 # Load external libreries
 import newspaper
+import json
+import os
 
 # Building a news source
-paper = newspaper.build('http://www.tnrelaciones.com/cm/phptn/enlaces_tn/193/206/', language='es')
+paper = newspaper.build('https://www.diariodehuelva.es/', language='es', memoize_articles=False)
 
 # Extracting articles
 for article in paper.articles:
@@ -51,16 +53,51 @@ print(article1.keywords)
 
 
 # # # # # # # # # #
-for key in urls_dict.keys():
-    print(key)
-    urls = urls_dict[key]
-    for url in urls:
-        url = url.split('_')
-        print(url[0])
-        paper = newspaper.build(url[1], language='es')
-        print(paper.brand)
-        print(paper.description)
-        print(paper.size())
+# for key in urls_dict.keys():
+#     print(key)
+#     urls = urls_dict[key]
+#     for url in urls:
+#         url = url.split('_')
+#         print(url[0])
+#         paper = newspaper.build(url[1], language='es')
+#         print(paper.brand)
+#         print(paper.description)
+#         print(paper.size())
+
+path = r'C:\Users\juan\news-please-repo\data\2020\06\06\huelvabuenasnoticias.com'
+
+j = 0
+base = ['authors', 'date_download', 'date_modify', 'date_publish', 'description', 
+        'filename', 'image_url', 'language', 'localpath', 'title', 'title_page', 
+        'title_rss', 'source_domain', 'maintext', 'url']
+for file in os.listdir(path):
+    if file[-5:] == '.json':
+        f = open(os.path.join(path, file), 'r')
+        art = json.load(f)
+        keys = list(art.keys())
+        if keys != base:
+            print('amos tu manco')
+    j += 1
+        
+    if j % 100:
+        print(j)
+        
+        
+
+# pre = r'C:\Users\juan\news-please-repo\data\2020\06\06'
+# for i in range(10):
+#     new_folder = os.path.join(pre, '_'+str(i))
+#     if not os.path.exists(new_folder):
+#         os.mkdir(new_folder)
+     
+#     j = 0
+#     while j < 
+#     if i != 9:
+        
+#     else:
+    
+
+# files = os.listdir(path)
 
         
 
