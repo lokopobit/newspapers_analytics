7bf3e279d9efe80473cf70f1faf1e40b5869b435
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-06-11 18:00:13 +0200

    Add: cleaning function
    Delete json files throwing error when opening.

--- get_news.py
+++ get_news.py
@@ -9,49 +9,63 @@ Created on Sat May 23 16:55:43 2020
 import json
 import os
 
-path = r'C:\Users\juan\news-please-repo\data\2020\06\06\huelvabuenasnoticias.com'
+#
+def split_folder(): 
+    pre = r'C:\Users\juan\news-please-repo\data\2020\06\06'
+    for i in range(10):
+        new_folder = os.path.join(pre, '_'+str(i))
+        if not os.path.exists(new_folder):
+            os.mkdir(new_folder)
+         
+        j = 0
+        while j < 200:
+            if i != 9:
+                pass
+            else:
+                pass
+    
+#
+def json_keys_checking(path, base, test=False):
+    error_files = []   
+    j = 0
+    for file in os.listdir(path):
+        if file[-5:] == '.json':
+            try:
+                f = open(os.path.join(path, file), 'r')
+                art = json.load(f)
+                f.close()
+            except:
+                error_files.append(file)
+                continue
+            keys = list(art.keys())
+            if keys != base:
+                print('amos tu manco')
+        j += 1
+        
+        if test and j >10:          
+            break
+        
+        if j % 10000 == 0:
+            print(j)
+    return error_files
 
-j = 0
+#
+def remove_error_files(path, error_files):
+    for ef in error_files: os.remove(os.path.join(path, ef))
+    print('Removed ', len(error_files), ' files')
+        
 base = ['authors', 'date_download', 'date_modify', 'date_publish', 'description', 
         'filename', 'image_url', 'language', 'localpath', 'title', 'title_page', 
         'title_rss', 'source_domain', 'maintext', 'url']
-error_files = []
-for file in os.listdir(path):
-    if file[-5:] == '.json':
-        try:
-            f = open(os.path.join(path, file), 'r')
-            art = json.load(f)
-            f.close()
-        except:
-            error_files.append(file)
-            continue
-        keys = list(art.keys())
-        if keys != base:
-            print('amos tu manco')
-    j += 1
-    
-    # if (j >10):
-    #     break
-    
-    if j % 10000 == 0:
-        print(j)
-        
-        
+path = r'C:\Users\juan\news-please-repo\data\2020\06\06\huelvabuenasnoticias.com'
+path = r'C:\Users\juan\news-please-repo\data\2020\06\10\diariodehuelva.es'
+path = r'C:\Users\juan\news-please-repo\data\2020\06\11\diariodehuelva.es'
 
-# pre = r'C:\Users\juan\news-please-repo\data\2020\06\06'
-# for i in range(10):
-#     new_folder = os.path.join(pre, '_'+str(i))
-#     if not os.path.exists(new_folder):
-#         os.mkdir(new_folder)
-     
-#     j = 0
-#     while j < 
-#     if i != 9:
-        
-#     else:
+# Newspaaper cleaning
+error_files = json_keys_checking(path, base, test=False)
+remove_error_files(path, error_files)
     
 
-# files = os.listdir(path)
 
         
 
