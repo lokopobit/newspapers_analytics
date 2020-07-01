6b0cfcfcc1189a6be4a9415e208537b8284685fe
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-07-01 19:53:29 +0200

    Add: remove duplicated articles befores inserting in mongo db
    already_cleaned.json now contaains a dict per newspaper, that contains paths and unique urls.
    
    Also more verbore when cleaning is included.

--- get_news.py
+++ get_news.py
@@ -6,13 +6,9 @@ Created on Sat May 23 16:55:43 2020
 """
 
 # Load external libreries
-from multiprocessing import Pool, Lock
 from pymongo import MongoClient
 import win32com.shell.shell as shell
-import json, hjson
-from time import sleep
-import configparser
-import os
+import os, json
 import numpy as np
 
 #
@@ -32,52 +28,51 @@ def fast_dir_walking(data_path):
                     dirs_dict[new_key].append(newsp) # Remove data_path from newsp?
     return dirs_dict
                         
-#
-def split_folder(): 
-    pre = r'C:\Users\juan\news-please-repo\data\2020\06\06'
-    for i in range(10):
-        new_folder = os.path.join(pre, '_'+str(i))
-        if not os.path.exists(new_folder):
-            os.mkdir(new_folder)
-         
-        j = 0
-        while j < 200:
-            if i != 9:
-                pass
-            else:
-                pass
     
 #
-def json_keys_checking(path, base, test=False):
-    error_files = []  
+def cleaning(newsp_path, urls, test=False):
+    
+    base = ['authors', 'date_download', 'date_modify', 'date_publish', 'description', 
+            'filename', 'image_url', 'language', 'localpath', 'title', 'title_page', 
+            'title_rss', 'source_domain', 'maintext', 'url']
+    
+    error_files, different_files = [], []
     j = 0
-    for file in os.listdir(path):
+    for file in os.listdir(newsp_path):
         if file[-5:] == '.json':
             try:
-                f = open(os.path.join(path, file), 'r')
+                f = open(os.path.join(newsp_path, file), 'r')
                 art = json.load(f)
                 f.close()
             except:
                 error_files.append(file)
                 continue
+            
             keys = list(art.keys())
             if keys != base:
-                print('amos tu manco')
-            # if file.count('_') <= 2: # non articles
-            #     error_files.append(file)
+                different_files.append(file)
+                continue
+            
+            if art['url'] in urls:
+                error_files.append(file)
+                continue
+            else:
+                urls.append(art['url'])
+            
+
         j += 1
         
         if test and j >10:          
             break
         
         if j % 10000 == 0:
-            print(j)
-    return error_files
+            print('Files checked:', j)
+            
+    return error_files, different_files, urls
 
 #
-def remove_error_files(path, error_files):
-    for ef in error_files: os.remove(os.path.join(path, ef))
-    print('Removed ', len(error_files), ' files')
+def remove_error_files(newsp_path, error_files):
+    for ef in error_files: os.remove(os.path.join(newsp_path, ef))
     
 #
 def find_duplicated_articles(paths):
@@ -92,88 +87,9 @@ def find_duplicated_articles(paths):
                 aux = np.intersect1d(f1, f2)
                 print(aux)
 
-#
-def find_article_years(path):
-    years = []
-    for file in os.listdir(path):
-        if file[-5:] == '.json':
-            f = open(os.path.join(path, file), 'r')
-            art = json.load(f)
-            f.close()
-            years.append(int(art['date_publish'][:4]))
-    return years
 
-def main():
-    store = True
-    base = ['authors', 'date_download', 'date_modify', 'date_publish', 'description', 
-            'filename', 'image_url', 'language', 'localpath', 'title', 'title_page', 
-            'title_rss', 'source_domain', 'maintext', 'url']
-    
-    data_path = r'C:\Users\juan\news-please-repo\data'
-    dirs_dict = fast_dir_walking(data_path)
-    f = open('already_cleaned.json', 'r') ; already_cleaned = json.load(f) ; f.close()
-    
-    # Newspaaper cleaning
-    for key_ in dirs_dict.keys():
-        for newsp in dirs_dict[key_]:
-            print(newsp)
-            if key_ not in already_cleaned.keys():
-                already_cleaned[key_] = []
-                
-            if newsp in already_cleaned[key_]:
-                print('Already cleaned')
-                continue
-            error_files = json_keys_checking(newsp, base, test=False)
-            remove_error_files(newsp, error_files)
-        # find_duplicated_articles(dirs_dict[key_])
-        
-    f = open('already_cleaned.json', 'w') ; json.dump(dirs_dict,f) ; f.close()
-        
-    if store:
-        f = open('already_stored.json', 'r') ; already_stored = json.load(f) ; f.close()
-        insert2mongo(dirs_dict, already_stored)
-        f = open('already_stored.json', 'w') ; json.dump(dirs_dict,f) ; f.close()
-        
 
-#    
-def execute_newsplease_cli(newspaper_url):
-    config_path = r'C:\Users\juan\news-please-repo\config'
-    general_config = 'config.cfg'
-    news_config = 'sitelist.hjson'
-    
-    with lock:
-        sleep(10)
-        f = open(os.path.join(config_path, news_config), 'r')
-        nc = hjson.load(f)
-        nc['base_urls'][0]['url'] = newspaper_url
-        f.close()
-        
-        f = open(os.path.join(config_path, news_config), 'w')
-        hjson.dump(nc, f)
-        f.close()
-        
-        config = configparser.RawConfigParser()
-        config.read(os.path.join(config_path,general_config))
-        config.set('Scrapy', 'JOBDIRNAME', 'jobdir'+newspaper_url.split('.')[1])
-        with open(os.path.join(config_path,general_config), 'w') as configfile:
-            config.write(configfile)
-        
-    os.system('cmd /k "news-please"')
 
-#        
-def init_child(lock_):
-    global lock
-    lock = lock_
- 
-#
-def multiprocess(n_pools):
-    lock = Lock()
-    p = Pool(n_pools, initializer=init_child, initargs=(lock,))
-    # p.starmap(ca, [(3,4), (1,3)])
-    urls = ['https://www.diariodehuelva.es/', 'https://www.huelvabuenasnoticias.com/', 'http://huelva24.com/']
-    urls = ['https://huelvaya.es/', 'https://www.huelvainformacion.es/', 'http://www.huelvahoy.com/']
-    p.map(execute_newsplease_cli, urls)
-    
 #
 def insert2mongo(dirs_dict, already_stored):
     # os.system('cmd /k "C:\\mongodb\\bin\\mongod.exe"')    
@@ -197,9 +113,51 @@ def insert2mongo(dirs_dict, already_stored):
                     f = open(os.path.join(newsp, file), 'r')
                     art = json.load(f)
                     f.close()
-                    collection.insert_one(art)  
+                    # collection.insert_one(art)  
+                    collection.update(art, art, upsert=True)
     client.close()
     shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+'net stop MongoDB')
+
+
+#
+def main():
+    store = False
+    
+    data_path = r'C:\Users\juan\news-please-repo\data'
+    newsp_paths_dict = fast_dir_walking(data_path)
+    f = open('already_cleaned.json', 'r') ; already_cleaned = json.load(f) ; f.close()
+    
+    # Newspaaper cleaning
+    for newsp_key in newsp_paths_dict.keys():
+        print('-'*20)
+        print('Cleaning', newsp_key)
+        print('-'*20)
+        for newsp_path in newsp_paths_dict[newsp_key]:
+            
+            if newsp_key not in already_cleaned.keys():
+                already_cleaned[newsp_key] = {}
+                already_cleaned[newsp_key]['paths'] = []
+                already_cleaned[newsp_key]['urls'] = []
+                
+            if newsp_path in already_cleaned[newsp_key]['paths']:
+                print(newsp_path.replace(data_path, ''), 'Already cleaned')
+                continue
+            
+            urls_ = already_cleaned[newsp_key]['urls']
+            error_files, different_files, urls = cleaning(newsp_path, urls_, test=False)
+            already_cleaned[newsp_key]['urls'] = urls
+            
+            remove_error_files(newsp_path, error_files)
+            already_cleaned[newsp_key]['paths'].append(newsp_path)
+            print(newsp_path.replace(data_path, ''), 'Cleaned.', 'Removed ', len(error_files), ' files')
+        
+    f = open('already_cleaned.json', 'w') ; json.dump(already_cleaned,f) ; f.close()
+        
+    if store:
+        f = open('already_stored.json', 'r') ; already_stored = json.load(f) ; f.close()
+        insert2mongo(dirs_dict, already_stored)
+        f = open('already_stored.json', 'w') ; json.dump(dirs_dict,f) ; f.close()
+        
 
 #
 def mongoQueries():
@@ -216,13 +174,15 @@ def mongoQueries():
         aux=[]
         for aa in a:
             aux.append(aa['url'])        
-        return len(aux), len(np.unique(aux))
+        return len(aux), len(np.unique(aux)), aux
     
     shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+'net start MongoDB')    
     client = MongoClient()
     db = client.newsHuelva
     
-    newsp = db[db.list_collection_names()[3]]
+    collection = db.list_collection_names()[2]
+    newsp = db[collection]
+    print(collection)
     print(newsp.estimated_document_count())
  
     # newsp.find_one()
@@ -248,8 +208,48 @@ def mongoQueries():
     
     
     
+# a=newsp.find()
+# for aa in a:
+#     print(aa)
+    
+# a=newsp.find({'url':'http://huelva24.com/art/70727/rafael-segovia-ldquo-a-los-onubenses-poder-decir-que-somos-andaluces-nos-sale-muy-caro-rdquo-'})
+# for aa in a:
+#     # print('*'*90)
+#     print(aa['description'])  
+    
+# client.drop_database('newsHuelva')
+    
+# [item for item, count in collections.Counter(a).items() if count > 1]
+    
+    
+    
+    
+    
+    
+    
+#
+def find_article_years(path):
+    years = []
+    for file in os.listdir(path):
+        if file[-5:] == '.json':
+            f = open(os.path.join(path, file), 'r')
+            art = json.load(f)
+            f.close()
+            years.append(int(art['date_publish'][:4]))
+    return years    
 
-
-        
-
-
+   
+#
+def split_folder(): 
+    pre = r'C:\Users\juan\news-please-repo\data\2020\06\06'
+    for i in range(10):
+        new_folder = os.path.join(pre, '_'+str(i))
+        if not os.path.exists(new_folder):
+            os.mkdir(new_folder)
+         
+        j = 0
+        while j < 200:
+            if i != 9:
+                pass
+            else:
+                pass
