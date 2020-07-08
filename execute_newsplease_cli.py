8a4f8f85c405307bccbad78e3a021d08a248a2b5
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-07-08 18:41:12 +0200

    Add: kill() function and auxiliary txt
    With the objective of making automatic the multiprocess() function so that it can scrape every url in prensa's jsons, the os.system() command was replaced with subprocess.Popen() command and a kill function was included. The kill functions kills the launched processes that scrape newspapers so that another newspapers can be scraped.
    
    The auxiliary txt is needed to save the proceses pids.

--- execute_newsplease_cli.py
+++ execute_newsplease_cli.py
@@ -8,11 +8,15 @@ Created on Tue Jun 30 12:11:55 2020
 from multiprocessing import Pool, Lock
 import hjson, json
 from time import sleep
+import subprocess
+import psutil
 import configparser
 import os
 
+procs_pid = []
 #    
 def execute_newsplease_cli(newspaper_url):
+    # global procs_pid
     config_path = r'C:\Users\juan\news-please-repo\config'
     general_config = 'config.cfg'
     news_config = 'sitelist.hjson'
@@ -34,7 +38,13 @@ def execute_newsplease_cli(newspaper_url):
         with open(os.path.join(config_path,general_config), 'w') as configfile:
             config.write(configfile)
         
-    os.system('cmd /k "news-please"')
+    # os.system('cmd /k "news-please"')
+    proc=subprocess.Popen(['news-please'], stdout=subprocess.PIPE, shell=False)
+    f = open('prueba.txt', 'a')
+    f.write('\n '+str(proc.pid))
+    f.close()
+    procs_pid.append(proc.pid)
+    print(procs_pid)
 
 #        
 def init_child(lock_):
@@ -43,6 +53,12 @@ def init_child(lock_):
  
 #
 def multiprocess(n_pools):
+
+    def kill(proc_pid):
+        process = psutil.Process(proc_pid)
+        for proc in process.children(recursive=True):
+            proc.kill()
+        process.kill()
     
     def create_newsp_urls_dict(urls):
         f = open('already_cleaned.json', 'r') ; already_cleaned = json.load(f) ; f.close()
@@ -54,13 +70,30 @@ def multiprocess(n_pools):
             f = open(key_+'.json', 'w')
             json.dump({key_:already_urls}, f)
             f.close()
-        
+       
     lock = Lock()
     p = Pool(n_pools, initializer=init_child, initargs=(lock,))
     # p.starmap(ca, [(3,4), (1,3)])
-    urls = ['https://www.diariodehuelva.es/', 'https://www.huelvabuenasnoticias.com/', 'http://huelva24.com/']
-    # urls = ['https://huelvaya.es/', 'https://www.huelvainformacion.es/', 'http://www.huelvahoy.com/']
-    urls = ['https://huelvaya.es/', 'https://www.huelvainformacion.es/']
+    # urls = ['https://www.diariodehuelva.es/', 'https://www.huelvabuenasnoticias.com/', 'http://huelva24.com/']
+    urls = ['https://huelvaya.es/', 'https://www.huelvainformacion.es/', 'http://www.huelvahoy.com/']
+    # urls = ['https://huelvaya.es/', 'https://www.huelvainformacion.es/']
     create_newsp_urls_dict(urls)
     p.map(execute_newsplease_cli, urls)
+    f = open('prueba.txt', 'r')
+    procs_pid = f.readlines()
+    f.close()
+    aux=[]
+    for ax in procs_pid:
+        try:
+            aux.append(int(ax.replace('\n', ' ')))
+        except:
+            continue
+
+    for proc_pid in aux: kill(proc_pid)
+    # os.remove('prueba.txt')
+
+
+
+
+
 
