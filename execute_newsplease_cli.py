5a61cd65a21796cbaef2159147ba3ba63d106f71
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-07-22 18:57:53 +0200

    Add: configuration file.
    configs_path.csv containing the database path and the newsplease library config file path.

--- execute_newsplease_cli.py
+++ execute_newsplease_cli.py
@@ -5,6 +5,7 @@ Created on Tue Jun 30 12:11:55 2020
 @author: lokopobit
 """
 
+import pandas as pd
 from multiprocessing import Pool, Lock
 import hjson, json
 from time import sleep
@@ -17,12 +18,15 @@ import os
 #    
 def execute_newsplease_cli(newspaper_url):
     # global procs_pid
-    config_path = r'C:\Users\juan\news-please-repo\config'
-    general_config = 'config.cfg'
-    news_config = 'sitelist.hjson'
+    # config_path = pd.read_csv('configs_path.csv')['config_path'].tolist()[0]
+    # general_config = 'config.cfg'
+    # news_config = 'sitelist.hjson'
     
     with lock:
         sleep(20)
+        config_path = pd.read_csv('configs_path.csv')['config_path'].tolist()[0]
+        general_config = 'config.cfg'
+        news_config = 'sitelist.hjson'
         f = open(os.path.join(config_path, news_config), 'r')
         nc = hjson.load(f)
         nc['base_urls'][0]['url'] = newspaper_url
@@ -40,7 +44,7 @@ def execute_newsplease_cli(newspaper_url):
         
     # os.system('cmd /k "news-please"')
     proc=subprocess.Popen(['news-please'], shell=False)
-    f = open('prueba.txt', 'a')
+    f = open('json_data/process_PIDs.txt', 'a')
     f.write('\n '+str(proc.pid))
     f.close()
 
@@ -59,13 +63,13 @@ def multiprocess(n_pools):
         process.kill()
     
     def create_newsp_urls_dict(urls):
-        f = open('already_cleaned.json', 'r') ; already_cleaned = json.load(f) ; f.close()
+        f = open('json_data/already_cleaned.json', 'r') ; already_cleaned = json.load(f) ; f.close()
         for url in urls:
             for key_ in already_cleaned.keys():
                 if url.find(key_) != -1:
                     already_urls = already_cleaned[key_]['urls']
                     break
-            f = open(key_+'.json', 'w')
+            f = open('json_data/' + key_+'.json', 'w')
             json.dump({key_:already_urls}, f)
             f.close()
        
@@ -80,13 +84,16 @@ def multiprocess(n_pools):
     for i in range(len(range_)):
         if range_[i] == len(all_urls):
             continue
+        
         urls = all_urls[range_[i]:range_[i+1]]
         create_newsp_urls_dict(urls)
         p.map(execute_newsplease_cli, urls)
+        
         sleep(60*n_min)
-        f = open('prueba.txt', 'r')
+        f = open('json_data/process_PIDs.txt', 'r')
         procs_pid = f.readlines()
         f.close()
+        
         aux=[]
         for ax in procs_pid:
             try:
@@ -100,7 +107,7 @@ def multiprocess(n_pools):
             except:
                 continue
             
-        os.remove('prueba.txt')
+        os.remove('json_data/process_PIDs.txt')
 
 
 
--- main.py
+++ main.py
@@ -5,7 +5,14 @@ Created on Mon Jul 20 18:14:06 2020
 @author: lokopobit
 """
 
+import pandas as pd
 import get_news
+import execute_newsplease_cli
+
+# Scrape data 
+n_pools = 3
+execute_newsplease_cli.multiprocess(n_pools)
+
 
 # Data cleaning and storing
 mongo_store = True
@@ -13,7 +20,7 @@ mongo_store = True
 print('*'*50)
 print('Step 1: Cleaning')
 print('*'*50)
-data_path = r'C:\Users\juan\news-please-repo\data'
+data_path = pd.read_csv('configs_path.csv')['data_path'].tolist()[0]
 newsp_paths_dict = get_news.fast_dir_walking(data_path)
 get_news.cleaning(data_path, newsp_paths_dict)
     
