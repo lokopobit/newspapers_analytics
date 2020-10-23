76e72a673f0d2df46069704689b3ce59a6554436
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-10-23 18:42:54 +0200

    Update todo.txt.
    Create auxiliar function to remove underscore form prensas files.

--- auxiliar_functions.py
+++ auxiliar_functions.py
@@ -12,6 +12,7 @@ from random import sample
 from operator import itemgetter 
 import json
 import os
+from urllib.parse import urlparse
 
 
 #######################################
@@ -151,6 +152,60 @@ def create_prensas_all():
     f.close()    
         
     return prensas_all
+
+
+
+def remove_underscore_from_prensas_files():
+    ###
+    ### DON'T RUN
+    ###
+    
+    prensas = os.listdir('json_data')
+    prensas = [pr for pr in prensas if pr[:7] == 'prensa_']
+    
+    for prensa in prensas:
+        f = open(os.path.join('json_data', prensa), 'r')
+        prensa_dict = json.load(f)
+        f.close()
+
+        new_prensa_dict = {}
+        for key_,values in prensa_dict.items():
+            new_prensa_dict[key_] = []
+            for val in values:
+                newspaper_name = '_'.join(val.split('_')[:-1])
+                newspaper_url = val.split('_')[-1]
+                aux = urlparse(newspaper_url)
+                if aux.netloc == '': 
+                    newspaper_name = val.split('_H')[0]
+                    newspaper_url = 'H' + val.split('_H')[1]
+                    aux = urlparse(newspaper_url)
+                new_prensa_dict[key_].append([newspaper_name,aux.netloc])
+                    
+        f = open(os.path.join('json_data', prensa), 'w')
+        json.dump(new_prensa_dict, f)
+        f.close()
+
+
+
+def remove_duplicated_urls_from_prensas_all():
+    f = open('json_data/prensas_all.json', 'r')
+    prensa_dict = json.load(f)
+    f.close()    
+    
+    all_urls =  []
+    for key_,values in prensa_dict.items():
+        for val in values:
+            url = val[1]
+            all_urls.append(url)
+
+    unique_urls = list(set(all_urls))
+    new_prensa_dict = {}
+    for key_,values in prensa_dict.items():
+        new_prensa_dict[key_] = []
+        for val in values:
+            url = val[1]
+            if url not in unique_urls:
+                new_prensa_dict[key_].append([val[0], url])
 
 
 
